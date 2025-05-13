779d2017b4262ac35ec610e8bf363800802f10c6
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-13 20:45:49 +0300

    created students table

--- index.sql
+++ index.sql
@@ -3,3 +3,14 @@ CREATE DATABASE carl;
 
 -- USE carl db;
 USE carl;
+
+-- Create Students table
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    DateOfBirth DATE,
+    Email VARCHAR(100) UNIQUE,
+    PhoneNumber VARCHAR(20),
+    Address VARCHAR(255)
+);
